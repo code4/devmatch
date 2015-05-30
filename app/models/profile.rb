73252f6ffa35d0eaed835b2c20ce35f43b55d0d3
Manoj Subramaniam <cmu.manoj@gmail.com>
2015-05-29 17:30:54 -0700

    Added image upload feature to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  	#validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes, :default_url => "/images/missing.png"
+  	validates_attachment_content_type :avatar, :content_type =>  /\Aimage\/.*\Z/
 end
